5facd8a76286335fa84434ac3e2d47582e189cc4
Alexey <alexey.rykhalskiy@gmail.com>
2019-10-26 13:09:03 +0400

    CharacterPositions.java

--- src/main/java/lesson06/charpos/CharacterPositions.java
+++ src/main/java/lesson06/charpos/CharacterPositions.java
@@ -3,6 +3,7 @@ package lesson06.charpos;
 import lesson04.warmup.Lesson4Task1;
 
 import java.util.*;
+import java.util.function.BiConsumer;
 
 public class CharacterPositions {
 
@@ -27,6 +28,49 @@ public class CharacterPositions {
     return positions;
   }
 
+  public void printOutput(Map<Character, List<Integer>> data) {
+    Set<Map.Entry<Character, List<Integer>>> entries = data.entrySet();
+    for (Map.Entry<Character, List<Integer>> item: entries) {
+      Character key = item.getKey();
+      List<Integer> positions = item.getValue();
+      int size = positions.size();
+
+      System.out.printf("letter: %c : %d : %s\n",
+          key, size, positions);
+    }
+  }
+
+  public void printOutput2(Map<Character, List<Integer>> data) {
+    data.forEach(new BiConsumer<Character, List<Integer>>() {
+      @Override
+      public void accept(Character ch, List<Integer> list) {
+        System.out.printf("letter: %c : %d : %s\n", ch, list.size(), list);
+      }
+    });
+  }
+
+  public void printOutput4(Map<Character, List<Integer>> data) {
+    BiConsumer<Character, List<Integer>> biConsumer = new BiConsumer<Character, List<Integer>>() {
+      @Override
+      public void accept(Character ch, List<Integer> list) {
+        System.out.printf("letter: %c : %d : %s\n", ch, list.size(), list);
+      }
+    };
+
+    data.forEach(biConsumer);
+  }
+
+  public void printOutput5(Map<Character, List<Integer>> data) {
+    BiConsumer<Character, List<Integer>> biConsumer =
+        (ch, list) -> System.out.printf("letter: %c : %d : %s\n", ch, list.size(), list);
+
+    data.forEach(biConsumer);
+  }
+
+  public void printOutput3(Map<Character, List<Integer>> data) {
+    data.forEach((ch, list) -> System.out.printf("letter: %c : %d : %s\n", ch, list.size(), list));
+  }
+
   public static void main(String[] args) {
     // 1. data obtaining
     Lesson4Task1 l4t1 = new Lesson4Task1();
@@ -36,6 +80,8 @@ public class CharacterPositions {
     Map<Character, List<Integer>> result = app.process(origin);
     // 3. output the result
     System.out.println(origin);
-    System.out.println(result);
+//    app.printOutput(result);
+//    app.printOutput2(result);
+    app.printOutput3(result);
   }
 }
