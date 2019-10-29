30a806c30717c0f95751707f38f22f58fcff67d2
Alexey <alexey.rykhalskiy@gmail.com>
2019-10-29 09:45:49 +0400

    lesson07 - warmup

--- src/main/java/lesson07/Lesson7WarmUp.java
+++ src/main/java/lesson07/Lesson7WarmUp.java
@@ -8,6 +8,19 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Lesson7WarmUp {
+
+  public static boolean isOdd(int val) {
+    return val % 2 != 0;
+  }
+
+  public static boolean isEven(int val) {
+    return ! isOdd(val);
+  }
+
+
+
+
+
   public static void main(String[] args) {
     List<Integer> origin = Stream
         .generate(() -> (int) (Math.random() * 100))
@@ -16,9 +29,10 @@ public class Lesson7WarmUp {
 
     Comparator<Integer> super_comparator =
         (i1, i2) -> {
-          if (i1 < i2) return -1;
-          else if (i2 < i1) return 1;
-          return 0;
+          if (isEven(i1) && isOdd(i2)) return 1;
+          else if (isEven(i1) && isEven(i2)) return i2-i1;
+          else if (isOdd(i1) && isOdd(i2)) return i1-i2;
+          else return -1;
     };
 
     Collections.sort(sorted, super_comparator);
