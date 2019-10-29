7462d0a6351b0efb082facd855762a457f1b3dca
Alexey <alexey.rykhalskiy@gmail.com>
2019-10-29 11:37:51 +0400

    ListOfThings.java

---
+++ src/main/java/lesson07/exceptions/ListOfThings.java
@@ -0,0 +1,17 @@
+package lesson07.exceptions;
+
+public class ListOfThings {
+
+  public int calcSum(String origin) {
+    if (origin == null) return 0;
+    String[] split = origin.split(" ");
+    int total = 0;
+    for (int i=0; i<split.length; i++) {
+      try {
+        total += Integer.parseInt(split[i]);
+      } catch (NumberFormatException ignore) {}
+    }
+    return total;
+  }
+
+}
---
+++ src/test/java/lesson07/exceptions/ListOfThingsTest.java
@@ -0,0 +1,58 @@
+package lesson07.exceptions;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ListOfThingsTest {
+
+  private ListOfThings c;
+
+  @Before
+  public void before() {
+    this.c = new ListOfThings();
+  }
+
+  @Test
+  public void calcSum1() {
+    String origin = null;
+    assertEquals(0, c.calcSum(origin));
+  }
+
+  @Test
+  public void calcSum2() {
+    String origin = "";
+    assertEquals(0, c.calcSum(origin));
+  }
+
+  @Test
+  public void calcSum3() {
+    String origin = "dfvdfv dfvdf";
+    assertEquals(0, c.calcSum(origin));
+  }
+
+  @Test
+  public void calcSum4() {
+    String origin = "dfdf";
+    assertEquals(0, c.calcSum(origin));
+  }
+
+  @Test
+  public void calcSum5() {
+    String origin = "1 2 3";
+    assertEquals(6, c.calcSum(origin));
+  }
+
+  @Test
+  public void calcSum6() {
+    String origin = "1 dfv 6";
+    assertEquals(7, c.calcSum(origin));
+  }
+
+  @Test
+  public void calcSum7() {
+    String origin = "1 9 dsvef";
+    assertEquals(10, c.calcSum(origin));
+  }
+}
