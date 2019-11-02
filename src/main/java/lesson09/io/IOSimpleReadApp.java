1e4c1850478fb41a4482795a86a68818c50c8a85
Alexey <alexey.rykhalskiy@gmail.com>
2019-11-02 13:32:55 +0400

    -- lesson 9 io

--- src/main/java/lesson09/io/IOSimpleReadApp.java
+++ src/main/java/lesson09/io/IOSimpleReadApp.java
@@ -4,13 +4,21 @@ import java.io.*;
 
 public class IOSimpleReadApp {
   public static void main(String[] args) throws IOException {
-    BufferedReader bw = new BufferedReader(
+    BufferedReader br = new BufferedReader(
         new FileReader(
             new File("data/_1234567890.txt")));
-    String line = bw.readLine();
-    // -------------
-    System.out.println(line);
-    // -------------
-    bw.close();
+    String line;
+    // way 1
+    while (true) {
+      line = br.readLine();
+      if (line == null) break;
+      System.out.println(line);
+    }
+    // way 2
+    while ((line = br.readLine()) != null) {
+      System.out.println(line);
+    }
+
+    br.close();
   }
 }
