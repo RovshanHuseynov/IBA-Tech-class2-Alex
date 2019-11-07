40f8e0cb5b1f1525f805138aca30bccbbc18b56d
Alexey <alexey.rykhalskiy@gmail.com>
2019-11-07 10:08:27 +0400

    lesson 12: warmup task

--- src/main/java/lesson12/ShoesPairs.java
+++ src/main/java/lesson12/ShoesPairs.java
@@ -3,11 +3,13 @@ package lesson12;
 public class ShoesPairs {
   int calc(String origin) {
     int intervals = 0;
+    int pairs = 0;
     for (int i = 0; i < origin.length(); i++) {
       switch (origin.charAt(i)) {
-        case 'L': break;
-        case 'R': break;
+        case 'L': pairs++; break;
+        case 'R': pairs--; break;
       }
+      if (pairs == 0) intervals++;
     }
     return intervals;
   }
