71dc175e20b2b408389a9a6b75a18aeae9fab1d6
Alexey <alexey.rykhalskiy@gmail.com>
2019-11-07 09:35:16 +0400

    lesson 12: warmup task

---
+++ src/main/java/lesson12/ShoesPairs.java
@@ -0,0 +1,14 @@
+package lesson12;
+
+public class ShoesPairs {
+  int calc(String origin) {
+    int intervals = 0;
+    for (int i = 0; i < origin.length(); i++) {
+      switch (origin.charAt(i)) {
+        case 'L': break;
+        case 'R': break;
+      }
+    }
+    return intervals;
+  }
+}
---
+++ src/test/java/lesson12/ShoesPairsTest.java
@@ -0,0 +1,41 @@
+package lesson12;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ShoesPairsTest {
+
+  private ShoesPairs c;
+
+  @Before
+  public void setUp() throws Exception {
+    this.c = new ShoesPairs();
+  }
+
+  @Test
+  public void calc1() {
+    assertEquals(1, c.calc("RL"));
+  }
+
+  @Test
+  public void calc2() {
+    assertEquals(1, c.calc("RRLRLRLRLRLL"));
+  }
+
+  @Test
+  public void calc3() {
+    assertEquals(1, c.calc("LLLRRR"));
+  }
+
+  @Test
+  public void calc4() {
+    assertEquals(2, c.calc("RLRL"));
+  }
+
+  @Test
+  public void calc5() {
+    assertEquals(4, c.calc("RLLRLLLRRRRL"));
+  }
+}
