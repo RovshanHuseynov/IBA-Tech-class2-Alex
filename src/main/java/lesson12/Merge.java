6b8fc827527d1f407ef5ec0cf5053ae80a454d6c
Alexey <alexey.rykhalskiy@gmail.com>
2019-11-12 11:58:55 +0400

    -- folder renaming -- lesson 12. Algorithms

--- src/main/java/lesson12/Merge.java
+++ src/main/java/lesson12/Merge.java
@@ -6,7 +6,7 @@ public class Merge {
   public static void main(String[] args) {
     int[] a = {1,3,5,7,13}; // sorted !
     int[] b = {2,4,6,20,40,100}; // sorted !
-    // O(a.length+b.length) = 11
+    // idea: O(a.length + b.length) = 11
     int[] c = merge(a,b);
     System.out.printf("a:%s\n", Arrays.toString(a));
     System.out.printf("b:%s\n", Arrays.toString(b));
@@ -15,7 +15,16 @@ public class Merge {
 
   private static int[] merge(int[] a, int[] b) {
     int[] data = new int[a.length+b.length];
-
+    int i=0, j=0, k=0;
+    while (i<a.length && j<b.length) {
+      if (a[i] < b[j]) {
+        data[k++]=a[i++];
+      } else {
+        data[k++]=b[j++];
+      }
+    }
+    while(i<a.length) data[k++] = a[i++];
+    while(j<b.length) data[k++] = b[j++];
     return data;
   }
 }
