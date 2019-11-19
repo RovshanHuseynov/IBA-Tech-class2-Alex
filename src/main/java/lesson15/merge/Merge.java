5e9f83e3afdf41540677d751b12a826ff931da72
Alexey <alexey.rykhalskiy@gmail.com>
2019-11-19 14:17:34 +0400

    XLinkedList.java merge

--- src/main/java/lesson15/merge/Merge.java
+++ src/main/java/lesson15/merge/Merge.java
@@ -19,7 +19,36 @@ public class Merge {
     XLinkedList.XItem current1 = head1;
     XLinkedList.XItem current2 = head2;
     XLinkedList.XItem current = head;
-
+    // 1. we dont have an indices, so we have to assign the new head
+    // no way. if-statement
+    if (current1.value < current2.value) {
+      head = current1;
+      current = head;
+      current1 = current1.next;
+    } else {
+      head = current2;
+      current = head;
+      current2 = current2.next;
+    }
+    // 2. rewire all the elements until both of the lists contain elements
+    while (current1 != null && current2 != null) {
+      if (current1.value < current2.value) {
+        current.next = current1;
+        current = current1;
+        current1 = current1.next;
+      } else {
+        current.next = current2;
+        current = current2;
+        current2 = current2.next;
+      }
+    }
+    // 3. rewire all residual elements from non-empty list
+    if (current1 != null) {
+      current.next = current1;
+    }
+    if (current2 != null) {
+      current.next = current2;
+    }
 
     return head;
   }
