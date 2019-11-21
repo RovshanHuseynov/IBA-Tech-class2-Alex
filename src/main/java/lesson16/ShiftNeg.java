e0a44e22d3af4c9e25f9f0781562ef8381eb1641
Alexey <alexey.rykhalskiy@gmail.com>
2019-11-21 09:37:13 +0400

    - warmup test

--- src/main/java/lesson16/ShiftNeg.java
+++ src/main/java/lesson16/ShiftNeg.java
@@ -1,7 +1,30 @@
 package lesson16;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class ShiftNeg {
   int[] shift(int[] origin) {
-    return origin;
+    int[] shifted = origin.clone();
+
+    List<Integer> negative_indexes =
+        IntStream.range(0, origin.length) // Stream<int>
+            .filter(idx -> origin[idx] < 0)
+            .boxed()                      // Stream<Integer>
+            .collect(Collectors.toList());
+
+    LinkedList<Integer> negative_shifted_indexes =
+        new LinkedList<>(negative_indexes);
+
+    negative_shifted_indexes.addFirst(
+        negative_shifted_indexes.pollLast());
+
+    IntStream.range(0, negative_indexes.size())
+        .forEach(idx -> shifted[negative_indexes.get(idx)] =
+            origin[negative_shifted_indexes.get(idx)]);
+
+    return shifted;
   }
 }
