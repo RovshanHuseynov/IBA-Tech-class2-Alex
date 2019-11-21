0709b6a504f04c3541e1ff741aea12e026bc8a9e
Alexey <alexey.rykhalskiy@gmail.com>
2019-11-21 11:27:56 +0400

    tree

--- src/main/java/lesson16/tree/XTree.java
+++ src/main/java/lesson16/tree/XTree.java
@@ -4,7 +4,30 @@ public class XTree {
   public XNode root;
 
   public void add(int value) {
-
+    if (root == null) {
+      root = new XNode(value);
+    } else {
+      XNode current = root;
+      while (true) {
+        if (value < current.value) {
+          if (current.left == null) {
+            current.left = new XNode(value);
+            break;
+          } else {
+            current = current.left;
+          }
+        } else if (value > current.value) {
+          if (current.right == null) {
+            current.right = new XNode(value);
+            break;
+          } else {
+            current = current.right;
+          }
+        } else {
+          break;
+        }
+      }
+    }
   }
 
   boolean contains(int value) {
