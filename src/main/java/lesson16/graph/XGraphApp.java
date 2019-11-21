025ea0050b285af901b7df4873fa3b78cf2a8df8
Alexey <alexey.rykhalskiy@gmail.com>
2019-11-21 13:11:24 +0400

    tree contains

---
+++ src/main/java/lesson16/graph/XGraphApp.java
@@ -0,0 +1,34 @@
+package lesson16.graph;
+
+public class XGraphApp {
+  public static void main(String[] args) {
+    XGraph g = new XGraph(10);// 0..9
+    g.add(0,1);
+    g.add(0,2);
+
+    g.add(1,3);
+    g.add(1,4);
+
+    g.add(2,5);
+    g.add(2,6);
+
+    g.add(3,4);
+    g.add(3,5);
+    g.add(3,6);
+    g.add(3,7);
+
+    g.add(3,8);
+    boolean b1 = g.check(3,8);
+    System.out.println(b1);
+
+    g.remove(3, 8);
+    boolean b2 = g.check(3,8);
+    System.out.println(b2);
+
+    g.add(3,9);
+
+    g.add(8,9);
+
+
+  }
+}
