b349a82f68ebee7adc2e2a555e40ffce9fc23602
Alexey <alexey.rykhalskiy@gmail.com>
2019-11-23 15:23:21 +0400

    TheSimplestServer.java

--- src/main/java/lesson17/web/AuthHash.java
+++ src/main/java/lesson17/web/AuthHash.java
@@ -1,8 +1,18 @@
 package lesson17.web;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class AuthHash implements Auth {
+
+  private final Map<String, String> db = new HashMap<String, String>() {{
+    put("user","123");
+    put("admin","234");
+    put("manager","345");
+  }};
+
   @Override
   public boolean check(String name, String passwd) {
-    return false;
+    return db.containsKey(name) && db.get(name).equals(passwd);
   }
 }
