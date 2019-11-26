f6859e7bcc1c20873b98ecc4b908de4e8942d1cb
Alexey <alexey.rykhalskiy@gmail.com>
2019-11-26 10:52:33 +0400

    -- warmup18

--- src/main/java/lesson18/warmup/ByeServlet.java
+++ src/main/java/lesson18/warmup/ByeServlet.java
@@ -17,9 +17,16 @@ public class ByeServlet extends HttpServlet {
 
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    String name = "Jim";
+    int number = 43;
+    // read that file
+    // replace $name -> "Jim"
+    // replace $number -> 43
+    // display that file
+
     try(PrintWriter w = resp.getWriter()) {
-      String name = data.getName();
-      w.printf("Bye, %s", name);
+      String name1 = data.getName();
+      w.printf("Bye, %s", name1);
     }
   }
 }
