78b1bb2acdd72ca2c6c592610a4ed28c58c9b611
Alexey <alexey.rykhalskiy@gmail.com>
2019-11-30 11:12:49 +0400

    -- calc again

--- src/main/java/lesson19/db/DbOps.java
+++ src/main/java/lesson19/db/DbOps.java
@@ -32,4 +32,20 @@ public class DbOps {
     }
     return data;
   }
+
+  public static void insert_op(int op1, int op2, String op, int result, int user) {
+    try {
+      Connection conn = DbConn.get();
+      String SQLI = "insert into operations (op1, op2, op, r, user_id) VALUES (?,?,?,?,?)";
+      PreparedStatement stmt = conn.prepareStatement(SQLI);
+      stmt.setInt(1, op1);
+      stmt.setInt(2, op2);
+      stmt.setString(3, op);
+      stmt.setInt(4, result);
+      stmt.setInt(5, user);
+      stmt.execute();
+    } catch (SQLException e) {
+      throw new RuntimeException("DbOpsEX!!!", e);
+    }
+  }
 }
