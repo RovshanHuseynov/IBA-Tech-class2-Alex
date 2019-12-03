9827879af468115df4eb85711db26f4a31936c6e
Alexey <alexey.rykhalskiy@gmail.com>
2019-12-03 10:07:54 +0400

    -- warmup + topics

--- src/main/java/lesson21/warmup/NestedAppOptimized.java
+++ src/main/java/lesson21/warmup/NestedAppOptimized.java
@@ -9,13 +9,15 @@ import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class NestedAppOptimized {
+
+  private static IntStream convert(int[][] data) {
+    return Arrays.stream(data).flatMapToInt(Arrays::stream);
+  }
+
   public static void main(String[] args) {
     int[][] origin = {{1,6,3}, {}, {5,4}, {7,8,2,9}, {}};
-    IntStream ints = Arrays.stream(origin).flatMapToInt(Arrays::stream);
-    IntSummaryStatistics ss = ints.summaryStatistics();
-    ints.sorted().forEach(System.out::println);
-
-    Optional<Integer> min = Stream.of(1, 2, 3).min((o1, o2) -> o1 - o2);
+    IntSummaryStatistics ss = convert(origin).summaryStatistics();
+    convert(origin).sorted().forEach(System.out::println);
 
     ss.getSum();
     ss.getMin();
@@ -23,5 +25,6 @@ public class NestedAppOptimized {
     ss.getAverage();
     ss.getCount();
 
+//    Optional<Integer> min = Stream.of(1, 2, 3).min((o1, o2) -> o1 - o2);
   }
 }
