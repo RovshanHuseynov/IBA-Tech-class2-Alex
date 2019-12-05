8d8e24da09000f2743915621effe20f56a81c1de
Alexey <alexey.rykhalskiy@gmail.com>
2019-12-05 09:47:49 +0400

    -- src/main/java/lesson23

--- src/main/java/lesson23/warmup/GetRidOfDuplicates.java
+++ src/main/java/lesson23/warmup/GetRidOfDuplicates.java
@@ -1,9 +1,15 @@
 package lesson23.warmup;
 
+import java.util.stream.Collectors;
+
 public class GetRidOfDuplicates {
 
-  private static String filter(final String toFilter) {
-    return toFilter;
+  private static String filter(final String origin) {
+    return origin.chars()
+        .distinct() // Stream<int>
+        .mapToObj(i -> (char)i)
+        .map(String::valueOf)
+        .collect(Collectors.joining());
   }
 
   public static void main(String[] args) {
