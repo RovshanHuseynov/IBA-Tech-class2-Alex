2b784da82637acfa2fa97e48600ac5f4b803efa5
Alexey <alexey.rykhalskiy@gmail.com>
2019-12-07 10:24:28 +0400

    -- OptionalExplanation.java

---
+++ src/main/java/lesson24/warmup/OptionalExplanation.java
@@ -0,0 +1,26 @@
+package lesson24.warmup;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public class OptionalExplanation {
+  public static void main(String[] args) {
+    Optional<Integer> optI = Optional.ofNullable(null);
+    Optional<String> optS = optI
+        .map(x -> x + 1)
+        .map(x -> x * 2)
+        .map(x -> x + "!")
+        .map(x -> String.format("<a href=\"%s\">Link</a>", x));
+//    String result0 = optS.get(); // problem if null
+    String result1 = optS.orElse("");
+    System.out.println(result1);
+//    String result2 = optS.orElseThrow(() -> new IllegalArgumentException("no value"));
+
+//    ANTI-PATTERN. DONT DO THAT
+//    String s = "";
+//    if (s1.isPresent()) {
+//      s = s1.get();
+//    }
+  }
+}
